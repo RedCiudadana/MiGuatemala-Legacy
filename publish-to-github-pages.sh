e8d0347b35f91943bc4a1abec39e4fafd13eb422
Hector Hurtarte <hectorh30@gmail.com>
2016-04-13 21:38:57 -0600

    WIP

--- publish-to-github-pages.sh
+++ publish-to-github-pages.sh
@@ -3,7 +3,17 @@ git branch -D gh-pages
 git push dev --delete gh-pages
 git checkout -b gh-pages
 ember build --environment production
-git rm app config tests ember-cli-build.js bower.json package.json testem.json .bowerrc .editorconfig .jshintrc .travis.yml
+git rm app \
+    config \
+    tests \
+    ember-cli-build.js \
+    bower.json \
+    package.json \
+    testem.json \
+    .bowerrc \
+    .editorconfig \
+    .jshintrc \
+    .travis.yml
 mv dist/* .
 git add .
 git commit -m "Publishing to github pages"
