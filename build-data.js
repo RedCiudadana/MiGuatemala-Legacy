ace558d256852af6cf4aef7adfba692654412d1f
Hector Hurtarte <hectorh30@gmail.com>
2016-05-19 17:46:55 -0600

    Implement denormalization of institucion, partidoPostulante and partidoActual

--- build-data.js
+++ build-data.js
@@ -8,10 +8,41 @@ var tabletop = Tabletop.init({
   callback: function(sheets, tabletop) {
     var funcionarios = sheets['funcionario'].elements;
     var instituciones = sheets['institucion'].elements;
+    var partidos = sheets['partido'].elements;
 
-    var result = {"data": []};
+    var resultObject = {'data': []};
 
+    funcionarios.forEach(function(funcionario) {
+      var institucion = instituciones.filter(function(institucion) {
+        return institucion.id === funcionario.institucion;
+      });
 
+      if (institucion) {
+        funcionario.institucion = institucion;
+      }
+
+      var partidoPostulante = partidos.filter(function(partido) {
+        return partido.id === funcionario.partidoPostulante;
+      });
+
+      if (partidoPostulante) {
+        funcionario.partidoPostulante = partidoPostulante;
+      }
+
+      var partidoActual = partidos.filter(function(partido) {
+        return partido.id === funcionario.partidoActual;
+      });
+
+      if (partidoActual) {
+        funcionario.partidoActual = partidoActual;
+      }
+
+      resultObject.data.push(funcionario);
+    });
+
+    var file = 'public/data/denormalized-data-congreso.json';
+    jsonfile.spaces = 4;
+    jsonfile.writeFileSync(file, resultObject);
 
     return;
   }
