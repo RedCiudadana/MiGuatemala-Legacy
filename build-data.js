c990de2fdec59c36ed0ea97021403ab04b1fe5ed
Hector Hurtarte <hectorh30@gmail.com>
2016-06-01 14:36:04 -0600

    Cambiar estructura de funcionario.comisiones

--- build-data.js
+++ build-data.js
@@ -3,9 +3,9 @@ var jsonfile = require('jsonfile');
 
 var spreadsheet = 'https://docs.google.com/spreadsheets/d/1DnOk0jXyC-bGPG1uSUvi94tJ9zJWSBNEZWaVUYtg3LI/pubhtml';
 
-var tabletop = Tabletop.init({
+Tabletop.init({
   key: spreadsheet,
-  callback: function(sheets, tabletop) {
+  callback: function(sheets) {
     var funcionarios = sheets['funcionario'].elements;
     var instituciones = sheets['institucion'].elements;
     var partidos = sheets['partido'].elements;
@@ -43,32 +43,37 @@ var tabletop = Tabletop.init({
         funcionario.partidoActual = partidoActual[0];
       }
 
+      // Considerar comisiones
+      var comisionesFuncionario = [];
+
       // Considerar comisión
       var comision = comisiones.filter(function(comision) {
         return comision.id === funcionario.comision;
       });
-
-      if (comision) {
-        funcionario.comision = comision[0];
-      }
 
       // Considerar comisión dos
       var comision2 = comisiones.filter(function(comision2) {
         return comision2.id === funcionario.comision2;
       });
 
-      if (comision2) {
-        funcionario.comision2 = comision2[0];
-      }
-
       // Considerar comisión tres
       var comision3 = comisiones.filter(function(comision3) {
         return comision3.id === funcionario.comision3;
       });
 
-      if (comision3) {
-        funcionario.comision3 = comision3[0];
+      if (comision) {
+        comisionesFuncionario.push(comision[0]);
       }
+
+      if (comision2) {
+        comisionesFuncionario.push(comision2[0]);
+      }
+
+      if (comision3) {
+        comisionesFuncionario.push(comision3[0]);
+      }
+
+      funcionario.comisiones = comisionesFuncionario;
 
       // Considerar distritos
       var distrito = distritos.filter(function(distrito) {
