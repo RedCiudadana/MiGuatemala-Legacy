8e52874b26dd0b11c82c8002633c716c5d50f5ea
Hector Hurtarte <hectorh30@gmail.com>
2017-04-25 16:51:55 -0600

    Update deploy target

--- config/deploy.js
+++ config/deploy.js
@@ -22,7 +22,7 @@ module.exports = function(deployTarget) {
   }
 
   ENV.git = {
-    repo: 'https://github.com/RedCiudadana/MiGuatemala.git',
+    repo: 'https://github.com/RedCiudadana/JusticiaAbiertaBeta.git',
     branch: 'gh-pages',
     worktreePath: '/tmp/deploy'
   };
