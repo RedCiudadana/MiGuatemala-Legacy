2af8835bf298638def51044b4967639abc6f6e61
Hector Hurtarte <hectorh30@gmail.com>
2017-05-15 17:50:07 -0600

    WIP

---
+++ app/routes/resultados.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    // return Ember.RSVP.all([
+    //   // Información general de perfil
+    //   spreadsheet
+    //     .fetch('perfil-informacion-general-configuracion')
+    //     .then((configuracionData) => {
+          
+    //     });
+  }
+});
